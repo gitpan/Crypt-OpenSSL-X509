3d436e514787f89ffa550114d19206cb695bd30b
Dan Sully <daniel@cpan.org>
2011-03-30 03:00:14 +0000

    Import of DANIEL/Crypt-OpenSSL-X509-1.7 from CPAN.
    gitpan-cpan-distribution: Crypt-OpenSSL-X509
    gitpan-cpan-version:      1.7
    gitpan-cpan-path:         DANIEL/Crypt-OpenSSL-X509-1.7.tar.gz
    gitpan-cpan-author:       DANIEL
    gitpan-cpan-maturity:     released

--- X509.pm
+++ X509.pm
@@ -5,7 +5,7 @@ use vars qw($VERSION @EXPORT_OK);
 use Exporter;
 use base qw(Exporter);
 
-$VERSION = '1.6';
+$VERSION = '1.7';
 
 @EXPORT_OK = qw(
   FORMAT_UNDEF FORMAT_ASN1 FORMAT_TEXT FORMAT_PEM FORMAT_NETSCAPE
@@ -82,6 +82,11 @@ sub Crypt::OpenSSL::X509::Extension::extKeyUsage {
   return @vals;
 }
 
+sub Crypt::OpenSSL::X509::is_selfsigned {
+  my $x509 = shift;
+
+  return $x509->subject eq $x509->issuer;
+}
 
 BOOT_XS: {
   require DynaLoader;
@@ -235,6 +240,10 @@ Signature algorithm name as a string.
 =item issuer_name ( )
 
 Return a Name object for the subject or issuer name. Methods for handling Name objects are given below.
+
+=item is_selfsigned ( )
+
+Return Boolean value if subject and issuer name are the same.
 
 =item as_string ( [ FORMAT ] )
 
